45fd7fdd556740907b188e116579ef398c1dd406
Stephen C <cheunste@gmail.com>
2021-05-25 22:28:33 -0700

    Single Page application Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,11 @@ import HomePage from "./HomeComponent";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishdetailComponent";
+import Contact from "./ContactComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotion";
+import { LEADERS } from "../shared/leaders";
 
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
@@ -15,15 +19,29 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS,
     };
   }
 
   render() {
+    const HomePage = () => {
+      return (
+        <HomePage
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
+          <Route path="/contactus" component={Contact} />
           <Route
             exact
             path="/menu"
