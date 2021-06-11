967bba7d18f18a6ab9c567c5fc82ff3e515de3f4
Stephen Cheung <cheunste@gmail.com>
2021-06-11 15:44:20 -0700

    Added ActionCreator, Action TYpes and fixed a bitching issue with redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -12,26 +12,36 @@ import About from "./AboutComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { addComment } from "../redux/ActionCreators";
+
+const mapStateToProps = (state) => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  addComment: (dishId, rating, author, comment) =>
+    dispatch(addComment(dishId, rating, author, comment)),
+});
 
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      leaders: LEADERS,
-      promotions: PROMOTIONS,
-    };
   }
 
   render() {
     const HomePage = () => {
       return (
         <Home
-          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
         />
       );
     };
@@ -40,13 +50,14 @@ class Main extends Component {
       return (
         <DishDetail
           dish={
-            this.state.dishes.filter(
+            this.props.dishes.filter(
               (dish) => dish.id === parseInt(match.params.dishId, 10)
             )[0]
           }
-          comemnts={this.state.comments.filter(
+          comemnts={this.props.comments.filter(
             (comment) => comment.dishId === parseInt(match.params.dishId, 10)
           )}
+          addComment={this.props.addComment}
         />
       );
     };
@@ -61,12 +72,12 @@ class Main extends Component {
           <Route
             exact
             path="/menu"
-            component={() => <Menu dishes={this.state.dishes} />}
+            component={() => <Menu dishes={this.props.dishes} />}
           />
           <Route
             exact
             path="/aboutus"
-            component={() => <About leaders={this.state.leaders} />}
+            component={() => <About leaders={this.props.leaders} />}
           />
 
           <Redirect to="/home" />
@@ -77,4 +88,4 @@ class Main extends Component {
   }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,6 +1,14 @@
 import { COMMENTS } from "../shared/comments";
+import * as ActionTypes from "./ActionTypes";
+
 export const Comments = (state = COMMENTS, action) => {
   switch (action.type) {
+    case ActionTypes.ADD_COMMENT:
+      var comment = action.payload;
+      comment.id = state.length;
+      comment.date = new Date().toISOString();
+      //Recall you don't actually modifiy state. That's why we're concatening it
+      return state.concat(comment);
     default:
       return state;
   }
